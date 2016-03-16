b825216c8bad6a1a91bafa20ad970a0ad3b6f9cb
wjt <wjt128@gmail.com>
2016-03-16 16:01:21 +0800

    enable babel es7

--- app/controllers.js
+++ app/controllers.js
@@ -1,7 +1,21 @@
+
+
 'use strict';
 var _ = require('lodash');
 
 var controllers = angular.module('controllers', []);
 controllers.controller('mainControl', ['$scope', function($scope) {
-  $scope.test_word = 'Hello';
+  async function sleep(timeout) {
+    return new Promise((resolve, reject) => {
+      setTimeout(function() {
+        resolve();
+      }, timeout);
+    });
+  }
+
+  (async function() {
+    console.log('Do some thing, ' + new Date());
+    await sleep(3000);
+    console.log('Do other things, ' + new Date());
+  })();
 }]);
